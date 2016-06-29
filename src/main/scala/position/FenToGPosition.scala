e555f17cb667a7828d5013b38b3584d8a3e0ba34
terredeciels <chekunive@gmail.com>
2016-06-29 17:50:54 +0200

    master_3 perft pass

--- src/main/scala/position/FenToGPosition.scala
+++ src/main/scala/position/FenToGPosition.scala
@@ -6,43 +6,36 @@ import org.chesspresso.position.Position
 import position.ICodage._
 
 object FenToGPosition {
-  var cp_etats: Array[Int] = null
+  var cp_etats: Array[Int] = _
+  def toGPosition(fen: String):GPositionS = toGPosition(new Position(fen))
 
-  def toGPosition(fen: String): GPositionS = {
-    toGPosition(new Position(fen))
-  }
-
-  private def toGPosition(position: Position): GPositionS = {
-    val gp: GPositionS = new GPositionS()
-    //gp.fen = position.getFEN
+  private def toGPosition(position: Position) = {
+    val gp = new GPositionS()
     cp_etats = new Array[Int](NB_CASES)
     for (caseO <- CASES64) {
       cp_etats(caseO) = position.getStone(caseO)
     }
-    val etats: Array[Int] = new Array[Int](NB_CELLULES)
-    var caseO: Int = 0
+    val etats = new Array[Int](NB_CELLULES)
+    var caseO = 0
     while (caseO < NB_CELLULES) {
-      {
+
         etats(caseO) = OUT
-      }
-      {
+
+
         caseO += 1
-        caseO - 1
-      }
+       // caseO - 1
+
     }
-    val itetats: ArrayIterator = new ArrayIterator(cp_etats)
-    var indice: Int = 0
+    val itetats = new ArrayIterator(cp_etats)
+    var indice = 0
     while (itetats.hasNext) {
-      {
-        val e: Integer = itetats.next.asInstanceOf[Integer]
-        etats(CASES117(indice)) = e
-        indice += 1
-      }
+     // val e = itetats.next
+      etats(CASES117(indice)) = itetats.next.asInstanceOf[Integer]
+      indice += 1
     }
     gp.etats = etats
-    gp.side = if (position.getToPlay == Chess.WHITE) BLANC
-    else NOIR
-    val cp_roques: Int = position.getCastles
+    gp.side = if (position.getToPlay == Chess.WHITE) BLANC else NOIR
+    val cp_roques = position.getCastles
     gp.roques(0) = (2 & cp_roques) == 2
     gp.roques(1) = (1 & cp_roques) == 1
     gp.roques(2) = (8 & cp_roques) == 8
